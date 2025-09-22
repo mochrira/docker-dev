7991fad3b501853dff2c6344609023b13427bef6
mochrira <mochrira@gmail.com>
2025-09-22 21:16:22 +0700

    adding hook for flexibility to prepare workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 NG_INSTALL="${NG_INSTALL:-NO}"
 NG_VERSION="${NG_VERSION:-15.2}"
 
-if [ $NG_INSTALL = YES ] 
-then
+if [ $NG_INSTALL = YES ]; then
     if [ -z $(npm list -g | grep @angular/cli@${NG_VERSION}) ]
     then
         echo "Installing Angular version ${NG_VERSION}..."
@@ -17,9 +16,12 @@ then
 fi
 
 DIR="/workspace/www"
-if [ ! -d "$DIR" ] 
-then
+if [ ! -d "$DIR" ]; then
     mkdir -p /workspace/www
+fi
+
+if [ -f "/before_startup.sh" ]; then
+  sh /before_startup.sh
 fi
 
 sudo /usr/bin/supervisord -n -c /etc/supervisord.conf
